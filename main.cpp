fc649bef4d25a0fa12e561d86024bab3c1fbfafa
Saussereau Dominique <domsau2@yahoo.fr>
2018-07-19 04:40:54 +0200

    Bug de cin corrigé !
    En français.

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <math.h>
+using namespace std;
+int main()
+{
+    srand (time(NULL));
+    int minNombre=1;
+    int maxNombre=10;
+    int nombreATrouver = rand() % maxNombre + minNombre;
+    int tentative = 0;
+    string tentativeS = "0";
+
+    int nbTentative = 0;
+    cout << "Plus ou moins, un classique de la programmation." << endl << endl;
+    cout << "Pour commencer, on est entre "<< minNombre << " et " << maxNombre << "." << endl;
+    do
+    {
+
+input:
+        try
+        {
+            // cin >> tentative;
+            getline(cin, tentativeS); // Pour éviter des bugs de non nombre.
+            tentative = stoi(tentativeS);
+        }
+        catch (const std::invalid_argument& e)
+        {
+            cout << "Un nombre, svp !" << endl;
+            goto input; // Pardon !
+        }
+
+        if (nombreATrouver > tentative)
+        {
+            cout << "C\'est plus !" << endl;
+            minNombre = max(tentative,minNombre);
+        }
+        if (nombreATrouver < tentative)
+        {
+            cout << "C\'est moins !" << endl;
+            maxNombre = min(tentative,maxNombre);
+        }
+        nbTentative++;
+        if (tentative != nombreATrouver)
+        {
+            cout << "On est actuellement entre " << minNombre << " et " << maxNombre << "." << endl;
+        }
+
+    }
+    while (tentative != nombreATrouver);
+    cout << "Bravo ! C\'est une belle victoire, avec seulement " << nbTentative << " tentatives." << endl;
+    return 0;
+}
+
