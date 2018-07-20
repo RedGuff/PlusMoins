717ea08e4bd63ecd913f2d2c0f25c942d76a5917
Saussereau Dominique <domsau2@yahoo.fr>
2018-07-20 03:13:56 +0200

    goto => do while.

--- main.cpp
+++ main.cpp
@@ -12,35 +12,39 @@ int main()
     int nombreATrouver = rand() % maxNombre + minNombre;
     int tentative = 0;
     string tentativeS = "0";
-
+    bool inputOk = true;
     int nbTentative = 0;
     cout << "Plus ou moins, un classique de la programmation." << endl << endl;
     cout << "Pour commencer, on est entre "<< minNombre << " et " << maxNombre << "." << endl;
     do
     {
+        do
+        {
+            inputOk = true; // Pour autre demande éventuelle.
+            try
+            {
+                // cin >> tentative;
+                getline(cin, tentativeS); // Pour éviter des bugs de non nombre.
+                tentative = stoi(tentativeS);
+            }
+            catch (const std::invalid_argument& e)
+            {
+                cerr << "Un nombre, svp !" << endl;
+                inputOk = false;
+            }
+        }
+        while (inputOk == false);
 
-input:
-        try
-        {
-            // cin >> tentative;
-            getline(cin, tentativeS); // Pour éviter des bugs de non nombre.
-            tentative = stoi(tentativeS);
-        }
-        catch (const std::invalid_argument& e)
-        {
-            cerr << "Un nombre, svp !" << endl;
-            goto input; // Pardon !
-        }
 
         if (nombreATrouver > tentative)
         {
             cout << "C\'est plus !" << endl;
-            minNombre = max(tentative,minNombre);
+            minNombre = max(tentative, minNombre);
         }
         if (nombreATrouver < tentative)
         {
             cout << "C\'est moins !" << endl;
-            maxNombre = min(tentative,maxNombre);
+            maxNombre = min(tentative, maxNombre);
         }
         nbTentative++;
         if (tentative != nombreATrouver)
